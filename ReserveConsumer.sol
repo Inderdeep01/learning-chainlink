c9c1858ff640f29486e1642ffd90f2f9fa18a063
inderdeep <inderdeepsidhu01@gmail.com>
2024-03-04 19:48:08 +0530

    Proof of Reserve

---
+++ ReserveConsumer.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import {AggregatorV3Interface} from "@chainlink/contracts@0.8.0/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract ReserveConsumerV3 {
+    AggregatorV3Interface internal reserveFeed;
+    constructor() {
+        reserveFeed = AggregatorV3Interface(
+            0xa81FE04086865e63E12dD3776978E49DEEa2ea4e
+        );
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestReserve() public view returns (int) {
+        // prettier-ignore
+        (
+            /*uint80 roundID*/,
+            int reserve,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = reserveFeed.latestRoundData();
+
+        return reserve;
+    }
+}
